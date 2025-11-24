485d75c328bb1d455baed62f35e3e33fdf722a72
JunhOpportunity <twinjyjh5@gmail.com>
2025-11-24 23:21:21 +0900

    feat($RESTAPI) : 특정 ID에 대한 요청과 Not Found 구현

--- 3-nestjs-server/src/posts/posts.controller.ts
+++ 3-nestjs-server/src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +10,41 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: '1',
+    title: 'title1',
+    content: 'content1',
+    likeCount: 100000,
+    commentCount: 100,
+  },
+  {
+    id: 2,
+    author: '2',
+    title: 'title2',
+    content: 'content2',
+    likeCount: 100000,
+    commentCount: 100,
+  },
+];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'author',
-      title: 'title',
-      content: 'content',
-      likeCount: 100000,
-      commentCount: 100,
-    };
+  getPosts(){
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id : string){
+    const post = posts.find((post:PostModel) => post.id === +id)
+
+    if(!post) {
+      throw new NotFoundException(); 
+    }
+
+    return post;
   }
 }
