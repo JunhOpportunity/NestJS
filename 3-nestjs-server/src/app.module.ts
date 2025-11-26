f53d7f90714f4181c5a0add0e185d469c3eb6f9c
JunhOpportunity <twinjyjh5@gmail.com>
2025-11-26 22:38:27 +0900

    chore($Docker) : 도커 기본 설정

--- 3-nestjs-server/src/app.module.ts
+++ 3-nestjs-server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
+import { PostModel } from './posts/entities/posts.entity';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [PostModel],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
