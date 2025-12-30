13d5a3801fd293ce8e6a881d9864d2c2268573a6
JunhOpportunity <twinjyjh5@gmail.com>
2025-12-31 00:00:12 +0900

    feat($typeorm) : 기본세팅

---
+++ 4-typeorm/src/entity/user.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from "typeorm";
+
+@Entity()
+export class UserModel{
+  //ID
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  // 제목
+  @Column()
+  title: string;
+
+  // 데이터 생성 일자
+  @CreateDateColumn()
+  createdAt: Date;
+
+  // 데이터 업데이트 일자
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  // 버전관리
+  @VersionColumn()
+  version: number;
+
+  @Column()
+  @Generated('increment')
+  additionalId: number;
+}
