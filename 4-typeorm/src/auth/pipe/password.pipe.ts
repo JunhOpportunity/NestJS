d022931c277690fca2c935064fb2e2c7298c926d
JunhOpportunity <twinjyjh5@gmail.com>
2026-01-08 20:52:49 +0900

    feat($pipe) : Nest 파이프 구현 (유효성 검사)

---
+++ 4-typeorm/src/auth/pipe/password.pipe.ts
@@ -0,0 +1,43 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+
+@Injectable()
+export class PasswordPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length < 8) {
+      throw new BadRequestException('비밀번호는 최소 8자 이상이어야 합니다.');
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length > this.length) {
+      throw new BadRequestException(`최대 길이는 ${this.length}자 입니다.`);
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length < this.length) {
+      throw new BadRequestException(`최소 길이는 ${this.length}자 입니다.`);
+    }
+
+    return value.toString();
+  }
+}
